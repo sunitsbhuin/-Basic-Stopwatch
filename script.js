86105cfdc02fbb1cd3a0dd48c107578a851bf31c
sunitsbhuin <sunitbhuin@gmail.com>
2025-09-01 21:39:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+let timerDisplay = document.querySelector('.timerDisplay');
+
+let StopBtn = document.getElementById('StopBtn');
+
+let StartBtn = document.getElementById('StartBtn');
+
+let ResetBtn = document.getElementById('ResetBtn');
+
+let msec = 00 ;
+let secs = 00 ;
+let mins = 00 ;
+
+let timerId = null;
+
+StartBtn.addEventListener('click', function(){
+    if(timerId !== null){
+        clearInterval(timerId);
+    }
+    timerId = setInterval(startTimer, 10);
+});
+
+StopBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+});
+
+ResetBtn.addEventListener('click', function(){
+    clearInterval(timerId);
+    timerDisplay.innerHTML = '00 : 00 : 00';
+    msec = secs = mins = 00;
+});
+
+function startTimer(){
+    msec++;
+    if(msec == 100){
+        msec = 0;
+        secs++;
+        if(secs == 60){
+            secs = 0;
+            mins++;
+        }
+    }
+
+    let msecString = msec < 10 ? `0${msec}` : msec;
+    let secsString = secs < 10 ? `0${secs}` : secs;
+    let minsString = mins < 10 ? `0${mins}` : mins;
+
+
+    timerDisplay.innerHTML = `${minsString} : ${secsString} : ${msecString}`;
+}
+
